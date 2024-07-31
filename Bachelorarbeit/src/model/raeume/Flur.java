6f4e45d9fabbeac2bb34e45518e81ab84d612140
Maxim Frank <maxim.frank132@gmail.com>
2024-07-31 17:24:44 +0200

    Gitignore

--- Bachelorarbeit/src/model/raeume/Flur.java
+++ Bachelorarbeit/src/model/raeume/Flur.java
@@ -1,5 +1,16 @@
 package model.raeume;
 
-public class Flur {
+import java.util.List;
 
+import model.Moebelstueck;
+import model.Raum;
+import model.Tuer;
+
+public class Flur extends Raum{
+
+	public Flur(double laenge, double breite, List<Moebelstueck> moebel, List<Tuer> tueren) {
+		super(laenge, breite, moebel, tueren);
+	}
+
+	
 }
